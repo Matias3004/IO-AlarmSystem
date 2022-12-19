586b70b9339849ef3d31cdd61d94f62a72e245d7
Mateusz Muzioł <matias3004@gmail.com>
2022-12-19 20:54:35 +0100

    Fixed interface

--- src/main/java/AlarmSystem/Sensor/ISensor.java
+++ src/main/java/AlarmSystem/Sensor/ISensor.java
@@ -1,11 +1,5 @@
 package AlarmSystem.Sensor;
 
 public interface ISensor {
-    int ID = 0;
-    double threshold = 512;
-    double signal = 0.0;
-
-    double getThreshold();
-    void setThreshold(double threshold);
     double readSignal();
 }
